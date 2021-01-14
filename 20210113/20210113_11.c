1e709096a3421bfe68dbf65c357f4933c57ab69f
veselin4000 <veselinn.grigorov@gmail.com>
2021-01-14 20:12:58 +0200

    edit

--- 20210113/20210113_11.c
+++ 20210113/20210113_11.c
@@ -7,39 +7,39 @@ double yoghurt = 0.50;
 double icecreamCone = 0.60;
 double candy = 1.50;
 double lollipop = 0.15;
-double tomatoWeight = 0;
-float flourWeight;
-float yoghurtWeight;
-float coneWeight;
-float candyWeight;
-float lollipopWeight;
+float tomatoWeight = 0;
+float flourWeight = 0;
+float yoghurtWeight=0;
+float coneWeight=0;
+float candyWeight=0;
+float lollipopWeight=0;
 //float weight;
 printf("Здравейте, добре дошли в нашия магазин! Днес предлагаме - пресни домати,брашно, кисело мляко, сладолед на фунийки и близалки.\n");
 printf("Колко килограма домати ще желаете? ");
 scanf("%f",&tomatoWeight);
-float TomatoTotal = tomatoWeight*tomato;
+float tomatoTotal = tomatoWeight*tomato;
 
-printf("Колко килограма домати ще желаете? ");
+printf("Колко килограма брашно ще желаете? ");
 scanf("%f",&flourWeight);
 float flourTotal = flourWeight*flour;
 
-printf("Колко килограма домати ще желаете? ");
+printf("Колко кисели млека ще желаете? ");
 scanf("%f",&yoghurtWeight);
 float yoghurtTotal = yoghurtWeight*yoghurt;
 
-printf("Колко килограма домати ще желаете? ");
+printf("Колко сладоледа ще желаете? ");
 scanf("%f",&coneWeight);
 float icecreamConeTotal = coneWeight*icecreamCone;
 
-printf("Колко килограма домати ще желаете? ");
+printf("Колко бонбони ще желаете? ");
 scanf("%f",&candyWeight);
 float candyTotal = candyWeight*candy;
 
-printf("Колко килограма домати ще желаете? ");
+printf("Колко близалки ще желаете? ");
 scanf("%f",&lollipopWeight);
 
 float lollipopTotal = lollipopWeight*lollipop;
 
-float alltotal = TomatoTotal + flourTotal +yoghurtTotal+icecreamConeTotal+candyTotal;
-printf("%.2f",alltotal);
+float alltotal = tomatoTotal + flourTotal +yoghurtTotal+icecreamConeTotal+candyTotal+lollipopTotal;
+printf("%.2f",alltotal );
 }
