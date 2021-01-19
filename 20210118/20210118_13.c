54a1197414dd25119047cdd4118e861dc7574660
veselin4000 <veselinn.grigorov@gmail.com>
2021-01-19 15:22:14 +0200

    upd

--- 20210118/20210118_13.c
+++ 20210118/20210118_13.c
@@ -14,4 +14,18 @@ int main() {
   scanf("%d", &nY);
   if ( nA > nB && nA !=0 ) {
   printf("&& Operator : Both conditions are true\n");
-}
+  }
+  if ( nX > nY || nY != 20) {
+  printf("|| Operator : Only one condition is true\n");
+  }
+  if ( ! (nA > nB && nB !=0 ) ) {
+  printf(" ! Operator : Both conditions are true\n");
+  } else {
+  printf("Both conditions are true.\n");
+  }
+  if (!( nA < nB && nB !=5 ) ) {
+  printf(" ! Operator : Both conditions are true\n");
+  } else {
+  printf("Both conditions are true.\n");
+  }
+} 
--- 20210118/20210118_4.c
+++ 20210118/20210118_4.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-  int x = 1;
-  int y = 0;
+  int x = 10;
+  int y = 10;
 
   int true = x&&y&x<<1; // с един и същи приоритет са. от дясно на ляво
   printf("%d\n", true);
 
-  true = x&&y^x<<1;  //
+  true = x&&y^x<<1;  
   printf("%d\n", true);
-
+  return 0;
 }
