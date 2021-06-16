94b4202d092da66ca1d3a9fc4a49036368a5d4a9
Veselin Grigorov <61278707+veselin4000@users.noreply.github.com>
2021-06-16 15:58:06 +0300

    Create file.c

---
+++ group_tasks/file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int biggestNum(int *arr){
+  int biggest =0;
+for (int i = 0; i < 5; i++){
+  if (biggest<arr[i]){
+        biggest=arr[i];
+  }
+
+}
+return biggest;
+
+}
+
+int main(int argc, char* argv[]){
+        int data[50];
+        for( int i=0;i<argc;i++){
+                data[i]=atoi(argv[i]);
+        }
+        printf("%d",biggestNum(data));
+}
